106bc4d1ec2674eba15636755a5cb31a609f37f4
Jeff Dziadulewicz <jeffdziad@gmail.com>
2023-05-01 23:28:49 -0500

    Add project files.

---
+++ TrueFalseQuizGestures/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TrueFalseQuizGestures
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private List<Question> questions = new List<Question>();
+        private int currentQuestion = 0;
+        private int score = 0;
+        private bool finished = false;
+
+        public MainPage()
+        {
+            questions.Add(new Question { Text = "An ant can lift 1,000 times its body weight.", Answer = false, Img = "antLift.jpg" });
+            questions.Add(new Question { Text = "Greenland is the largest island in the world.", Answer = true, Img = "greenland.jpg" });
+            questions.Add(new Question { Text = "Vatican City is the smallest country in the world.", Answer = true, Img = "vaticanCity.jpg" });
+            questions.Add(new Question { Text = "Cheesecake comes from Italy.", Answer = false, Img = "cheesecake.jpg" });
+            questions.Add(new Question { Text = "An astronaut has played golf on the moon.", Answer = true, Img = "astronaughtGolfing.jpg" });
+
+            InitializeComponent();
+
+            DisplayQuestion(currentQuestion);
+        }
+
+        private void DisplayQuestion(int index)
+        {
+            if (index >= questions.Count)
+            {
+                RevealScore();
+            }
+            else
+            {
+                Question q = questions.ElementAt(index);
+                QuestionImage.Source = q.Img;
+                QuestionHeader.Text = $"Question #{currentQuestion + 1}";
+                QuestionText.Text = q.Text;
+            }
+        }
+
+        private void RevealScore()
+        {
+            finished = true;
+            QuestionHeader.Text = "Results";
+            QuestionText.Text = $"You scored a {score}/{questions.Count}!";
+            QuestionImage.IsVisible = false;
+            SwipeMarkers.IsVisible = false;
+        }
+
+        private void SwipedRight(object sender, SwipedEventArgs e)
+        {
+            if(!finished)
+            {
+                if (questions.ElementAt(currentQuestion).Answer) score++;
+                DisplayQuestion(++currentQuestion);
+            }
+            
+        }
+
+        private void SwipedLeft(object sender, SwipedEventArgs e)
+        {
+            if(!finished)
+            {
+                if (!questions.ElementAt(currentQuestion).Answer) score++;
+                DisplayQuestion(++currentQuestion);
+            }
+        }
+    }
+}
---
+++ TrueFalseQuizGestures/Question.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TrueFalseQuizGestures
+{
+    public class Question
+    {
+        public string Text { get; set; }
+        public bool Answer { get; set; }
+        public string Img { get; set; }
+    }
+}
